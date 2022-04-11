1ba391e66df67a9cb3ff0f186c54e3e2eb397af0
unknown <mahmodulshahin665@gmail.com>
2022-04-11 22:56:17 +0600

    exploring stringify and parse

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-alert('added ')
+// JavaScript Object Notation
+// JSON
+const user = { id: 112, name: 'mahmodul shahin', job: 'web developer' };
+const stringified = JSON.stringify(user);
+// console.log('user -- ', user)
+// console.log('stringified -- ', stringified);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir road',
+    profit: 15000,
+    owner: {
+        name: 'Alia Bhatt',
+        profession: 'actor'
+    },
+    products: ['laptop', 'mobile', 'headphone'],
+    isExpensive: false,
+}
+const shopStringified = JSON.stringify(shop);
+// console.log('shop -- ', shop)
+console.log('shopStringified -- ', shopStringified);
+const converted = JSON.parse(shopStringified)
+console.log('converted -- ', converted)
