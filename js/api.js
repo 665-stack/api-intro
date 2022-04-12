0935e7d6d25599546c6b23c88e49ba0f3590507d
unknown <mahmodulshahin665@gmail.com>
2022-04-12 12:13:31 +0600

    add two more example

--- js/api.js
+++ js/api.js
@@ -3,3 +3,15 @@ function loadData() {
         .then(response => response.json())
         .then(data => console.log(data))
 }
+// another example
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+// one more example
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
