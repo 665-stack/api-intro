c1e7611d7e11c9b0cc7b4cd6274cbe8acf6a9c45
unknown <mahmodulshahin665@gmail.com>
2022-04-14 10:32:09 +0600

    exploring get vs post

--- js/post.js
+++ js/post.js
@@ -16,6 +16,23 @@ function displayPosts(posts) {
 
         `;
         postContainer.appendChild(div)
-        console.log(post)
+
     }
 }
+// add a function with get post
+function addAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new post',
+            body: 'This is my post ',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+addAPost()
